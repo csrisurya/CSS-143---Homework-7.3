360861c1c1d2853b47f2b789db40edb3ca26a8ab
Srisurya Chandramouli <csrisurya@gmail.com>
2020-03-13 12:09:56 -0700

    fixed junit error, test class working

--- src/AutoCompleteTests.java
+++ src/AutoCompleteTests.java
@@ -1,9 +1,8 @@
-import static org.junit.*;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 public class AutoCompleteTests {
     @Test
     public void testPop(){
-
+        assertTrue(true);
     }
 }
