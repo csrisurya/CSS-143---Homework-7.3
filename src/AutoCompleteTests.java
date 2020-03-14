3fbf9e8f407158c3233406f1868296c8387f789e
Srisurya Chandramouli <csrisurya@gmail.com>
2020-03-14 15:10:07 -0700

    fixed search() and finished tests

--- src/AutoCompleteTests.java
+++ src/AutoCompleteTests.java
@@ -1,8 +1,41 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 public class AutoCompleteTests {
+
     @Test
     public void testAutoComplete(){
-        assertTrue(true);
+        List<String> dict = new ArrayList<>();
+        dict.add("hello");
+        dict.add("high");
+        dict.add("seattle");
+        dict.add("seatac");
+        dict.add("see");
+        dict.add("hollow");
+        dict.add("how");
+        Trie trie = new Trie(dict);
+        String[][] expected = {
+                {"hello", "high", "hollow", "how"},
+                {"seatac", "seattle", "see"},
+                {"seatac", "seattle"},
+                {"hollow", "how"},
+                {}
+
+        };
+        String[] searchKeys = {"h", "se", "sea", "ho", "xyz"};
+
+        for(int i = 0; i < expected.length; i++){
+            List<String> actual = trie.search(searchKeys[i]);
+            Collections.sort(actual);
+            // needs to be sorted because the list returns the correct words but it isn't in alphabetical order
+            assertEquals(Arrays.asList(expected[i]), actual);
+        }
+
+
     }
 }
--- src/Trie.java
+++ src/Trie.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Trie {
     private TrieNode root;
-    public Trie(){
+    public Trie(List<String> dict){
         root = new TrieNode("");
+        for(String word : dict){
+            insert(word);
+        }
     }
     public void insert(String word){
         word = word.toLowerCase();
@@ -23,7 +27,8 @@ public class Trie {
         searchString = searchString.toLowerCase();
         List<String> result = new ArrayList<>();
         TrieNode current = root;
-        for(char letter : searchString.toCharArray()){
+        for(int i = 0; i < searchString.length(); i++){
+            char letter = searchString.charAt(i);
             if(current.children.containsKey(letter)){
                 current = current.children.get(letter);
             }
@@ -33,6 +38,7 @@ public class Trie {
         }
 
         findAllChildrenWords(current, result);
+
         return result;
     }
     private void findAllChildrenWords(TrieNode current, List<String> result){
@@ -40,26 +46,37 @@ public class Trie {
             result.add(current.prefix);
         }
         for(Character c : current.children.keySet()){
-            current = current.children.get(c);
-            findAllChildrenWords(current, result);
+            findAllChildrenWords(current.children.get(c), result);
         }
     }
-
+    public List<String> justStringComparison(List<String> dict, String searchString){
+        List<String> result = new ArrayList<>();
+        for(String string : dict){
+            if(string.startsWith(searchString)){
+                result.add(string);
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        Trie trie = new Trie();
-//      trie.insert("hello");
-//      trie.insert("high");
-      trie.insert("seattle");
-      trie.insert("seatac");
-      trie.insert("see");
-//      trie.insert("hollow");
-//      trie.insert("how");
+        List<String> dict = new ArrayList<>();
+        dict.add("hello");
+        dict.add("high");
+        dict.add("seattle");
+        dict.add("seatac");
+        dict.add("see");
+        dict.add("hollow");
+        dict.add("how");
+        Trie trie = new Trie(dict);
 
-        List<String> result = trie.search("s");
-        System.out.println(result.toString());
+        List<String> actual =trie.search("xyz");
+        System.out.println(actual.toString());
+        Collections.sort(actual);
+        System.out.println(actual.toString());
 
-        }
     }
+
+}
 
 
